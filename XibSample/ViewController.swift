fa8ba6d8d00b1dae1c659108358af9f6df3745a8
tk0929 <bluecoffin929@gmail.com>
2021-01-07 23:29:12 +0900

    [update] AutoLayoutの適用

--- XibSample/ViewController.swift
+++ XibSample/ViewController.swift
@@ -8,14 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let sampleView:SampleView = SampleView(frame: CGRect(x: 0.0, y: 0.0, width: view.bounds.width, height: view.bounds.height))
-        self.view.addSubview(sampleView)
-        
+       
         
     }
 
