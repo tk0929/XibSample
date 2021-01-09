c0738414ad2915058f64ef410ada35d38c555cff
tk0929 <bluecoffin929@gmail.com>
2021-01-09 22:40:05 +0900

    [update]レイアウトサンプルの追加

---
+++ XibSample/ViewSample2/SampleView2.swift
@@ -0,0 +1,47 @@
+//
+//  SampleView.swift
+//  XibSample
+//
+//  Created by t.koike on 2020/12/25.
+//
+
+import UIKit
+
+
+final class SampleView2: UIView {
+    
+    @IBOutlet weak var SampleLabel: UILabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        loadNib()
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        loadNib()
+        
+    }
+    
+    
+    private func loadNib() {
+        
+//        xibファイルの読み込み
+        let nib = UINib(nibName: "SampleView2", bundle: nil)
+        guard let view = nib.instantiate(withOwner: self, options: nil).first as? UIView else{return}
+        
+//        xibのビューのサイズをカスタムクラスのビューに合わせる
+        view.frame = self.bounds
+        
+//        xibから読み込んだビューをカスタムクラスのサブビューに追加
+        self.addSubview(view)
+        
+    }
+    
+    
+
+    
+}
